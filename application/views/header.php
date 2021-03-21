d0e396de9e893352c93a88ce89f33adf50f5bc79
root <root@ip-172-31-39-151.us-east-2.compute.internal>
2021-03-21 10:35:04 +0000

    test commit from remote

--- application/views/header.php
+++ application/views/header.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<!-- Theme Made By www.w3schools.com -->
-	<title>Pentity</title>
+	<title>Equipment</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
